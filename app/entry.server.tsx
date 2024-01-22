99f9ce8b6422316c284f94a21765bab95c981736
Chris Turner <1084919+caturner81@users.noreply.github.com>
2024-01-22 11:38:51 -0700

    header test

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -32,7 +32,7 @@ export default async function handleRequest(
   }
 
   responseHeaders.set('Content-Type', 'text/html');
-  responseHeaders.set('Content-Security-Policy', header);
+  responseHeaders.set('Content-Security-Policy', 'frame-ancestors: http://localhost:63342');
 
   return new Response(body, {
     headers: responseHeaders,
